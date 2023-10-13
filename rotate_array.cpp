de1bcbba00e052d78243f827bcecf5f21d5d2c85
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2023-10-13 23:12:44 +0530

    Question189

--- rotate_array.cpp
+++ rotate_array.cpp
@@ -6,11 +6,38 @@ int main()
 {
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
     int k = 2;
-    vector<int> temp;
-    for (int i = 0; i < nums.size(); i++)
+    int n = nums.size();
+    k = k % n;
+    // Reverse the first n - k elements
+    int start = 0;
+    int end = n - k - 1;
+    while (start < end)
     {
-        temp[(i + k) % nums.size()] = nums[i];
+        swap(nums[start], nums[end]);
+        start++;
+        end--;
     }
-    nums = temp;
+    // Reverse the last k elements
+    start = n - k;
+    end = n - 1;
+    while (start < end)
+    {
+        swap(nums[start], nums[end]);
+        start++;
+        end--;
+    }
+    // Reverse the entire array
+    start = 0;
+    end = n - 1;
+    while (start < end)
+    {
+        swap(nums[start], nums[end]);
+        start++;
+        end--;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
     return 0;
 }
