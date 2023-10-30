5fe82804f76d1d88a64d1377fb095469325b9135
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2023-10-30 22:00:50 +0530

    Question119

---
+++ pascal_triangle_II.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int rowIndex = 3;
+    vector<vector<int>> result;
+    result.push_back({1});
+    for (int i = 1; i <= rowIndex; i++)
+    {
+        vector<int> temp(i + 1, 1);
+        for (int j = 1; j < i; j++)
+            temp[j] = result[i - 1][j] + result[i - 1][j - 1];
+        result.push_back(temp);
+    }
+    for (int i = 0; i < result[rowIndex].size(); i++)
+        cout << result[rowIndex][i] << " ";
+    return 0;
+}
