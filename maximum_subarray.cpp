9ae46737f4ca18b769700b7a9c6ee424b276f973
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2023-11-01 21:32:58 +0530

    Question53

---
+++ maximum_subarray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5};
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        currentSum += nums[i];
+        if (currentSum > maxSum)
+        {
+            maxSum = currentSum;
+        }
+        if (currentSum < 0)
+        {
+            currentSum = 0;
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
+}
