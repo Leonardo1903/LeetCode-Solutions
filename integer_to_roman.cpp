45aee38cc19bdb6032cc055261f0916ed3f37264
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2023-11-16 15:03:27 +0530

    Question12

---
+++ integer_to_roman.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int num = 1994;
+    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    string tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    string hrns[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    string ths[] = {"", "M", "MM", "MMM"};
+    string ans = ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+    cout << ans << endl;
+    return 0;
+}
